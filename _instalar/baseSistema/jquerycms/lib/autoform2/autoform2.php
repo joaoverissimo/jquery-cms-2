4c157ac77bb24691bbfdb534d864f2456fae82fa
João Verissimo Ribeiro <joao-verissimo@hotmail.com>
2013-03-25 11:49:50 -0300

    autoform2 suporte ao ckeditor 4
    Ckeditor 4 esta muito melhor que o editor anterior. No futuro as
    atualizacoes deste editor serao simples pois foi criado um arquivo na
    pasta do ckeditor com as notas de atualização.
    (jquerycms\lib\autoform2\ckeditor\jquery_notas.txt)

--- _instalar/baseSistema/jquerycms/lib/autoform2/autoform2.php
+++ _instalar/baseSistema/jquerycms/lib/autoform2/autoform2.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'ckeditor/ckeditor.php';
 require_once 'autoform2-tabs.php';
 
 class autoform2 {
--- _instalar/baseSistema/jquerycms/lib/autoform2/ckeditor/config.js
+++ _instalar/baseSistema/jquerycms/lib/autoform2/ckeditor/config.js
@@ -1,22 +1,14 @@
-/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.skin = 'office2003';
-    //config.removePlugins =  'elementspath,enterkey,entities,forms,pastefromword,htmldataprocessor,specialchar' ;
-    //config.toolbar = 'Basic';
-    //'Templates'
-    CKEDITOR.config.toolbar = [
+CKEDITOR.editorConfig = function( config ) {
+    config.toolbar_Jquery = [
     ['PasteText','-','Bold','Italic'],
     ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
     ['Image','Table','-','Link', 'RemoveFormat','Source']
-    ] ;
+    ];
     
-    config.enterMode = CKEDITOR.ENTER_BR;
-    config.shiftEnterMode = CKEDITOR.ENTER_P;
+    config.toolbar = 'Jquery';
 };
