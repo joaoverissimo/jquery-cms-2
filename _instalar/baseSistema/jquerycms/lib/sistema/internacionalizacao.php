953d3b0a310a67b58f58957ae2a12c88e7a2d14a
João Verissimo Ribeiro <joao-verissimo@hotmail.com>
2013-06-10 11:36:38 -0300

    internacionalizacao - langs novas
    correcao para quando uma lingua nao exisita.

--- _instalar/baseSistema/jquerycms/lib/sistema/internacionalizacao.php
+++ _instalar/baseSistema/jquerycms/lib/sistema/internacionalizacao.php
@@ -81,8 +81,28 @@ class internacionalizacao {
         return $retorno;
     }
 
+    private static function recurse_copy($src, $dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while (false !== ( $file = readdir($dir))) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if (is_dir($src . '/' . $file)) {
+                    recurse_copy($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
     public static function CriarLocaleFile() {
         $dir = ___AppRoot . "locale/" . internacionalizacao::getCurrentLang() . "/";
+        if (!file_exists($dir)) {
+            $dir_pt = ___AppRoot . "locale/pt-br/";
+            self::recurse_copy($dir_pt, $dir);
+        }
+
         $dados = arquivos::lerDiretorio($dir, array("locale.csv"));
         if (issetArray($dados)) {
             $s = "";
